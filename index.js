53055a45acd2d149ff8ae8410b354a23a90bce86
polina karna <polina.karna@gmail.com>
2024-01-07 21:40:27 +0300

    linted index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-console.log("Hello, Hexlet!");
+// import _ from 'lodash';
 
-import _ from "lodash";
-console.log(_.last(['one', 'two']));
+// console.log('Hello, Hexlet!');
+// console.log(_.last(['one', 'two']));
+
+const fn = (a, b) => {
+  const c = a + b; return c;
+};
+fn(1, 3);
